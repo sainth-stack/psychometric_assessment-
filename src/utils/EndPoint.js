059d1e941d2ede01269fec52b3dad64ee454fe64
sainath-stylishop <sainath@stylishop.com>
2025-04-21 00:24:51 +0530

    updated

--- src/utils/EndPoint.js
+++ src/utils/EndPoint.js
@@ -1,3 +1,3 @@
-// export const ENDPOINT = "https://test.talentspotifyapp.com"
-export const ENDPOINT = "http://localhost:5001"
+export const ENDPOINT = "https://test.talentspotifyapp.com"
+// export const ENDPOINT = "http://localhost:5001"
 
